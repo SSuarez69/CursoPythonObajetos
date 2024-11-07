e0349cd7fcd1de1ac427797772f3fc7f6bf64982
Sergio <ssuarez@outlook.com.ar>
2024-11-07 18:19:41 -0300

    commit 07/11

--- ISP.py
+++ ISP.py
@@ -4,29 +4,36 @@ class Trabajador():
     @abstractstaticmethod
     def Trabajar(self):
         pass
-    
+
+class comendor():
     @abstractstaticmethod
     def Comer(self):
         pass
-    
+
+class dormidor():
     @abstractstaticmethod
     def dormir(self):
         pass
 
 
-class Humano():
+class Humano(Trabajador,comendor,dormidor):
     def trabajar(self):
-        pritnt("El humano esta trabajando")
+        print("El humano esta trabajando")
     
     def comer(self):
-        pprit("El Humano esta comiendo")
+        print("El Humano esta comiendo")
     
     def dormir(self):
         print("El humano esta durmiendo")
 
 
-class Robot():
+class Robot(Trabajador):
     def trabajar(self):
-        pritnt("El Robot esta trabajando")
+        print("El Robot esta trabajando")
 
 
+trabajador = Humano()
+trabajador.trabajar()
+
+robot = Robot()
+robot.trabajar()
